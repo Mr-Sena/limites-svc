c61d5d008e28d6549e8f87ee80e93800bc60f5bd
Mr-Sena <jsenna985@gmail.com>
2024-05-20 18:24:58 -0300

    Git init commit.

---
+++ src/main/java/com/machinery/limitessvc/LimitesSvcApplication.java
@@ -0,0 +1,13 @@
+package com.machinery.limitessvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitesSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitesSvcApplication.class, args);
+	}
+
+}
